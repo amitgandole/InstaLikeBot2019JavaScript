2dcd106e7de50b3c744668b793cbc5db9e11bb26
Amit Gandole <35072407+amitgandole@users.noreply.github.com>
2020-07-22 14:26:35 +0530

    Update InstaLikeBot.js

--- InstaLikeBot.js
+++ InstaLikeBot.js
@@ -1,6 +1,6 @@
 (() => {
   setInterval(()=>{
-    document.getElementsByTagName('button')[4].click();
+    document.getElementsByTagName('button')[4].click(); //PS : If button[4] is not working, search for index of like button and enter here. CrossCheck like button before running.
     document.querySelector('a.coreSpriteRightPaginationArrow').click();
 },3000)
 })()
